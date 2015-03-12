5c462ceeb523651f60b616f16079f2e8dd780575
Roland Bali <roland.bali@spagettikod.se>
2015-03-12 20:52:35 +0100

    Converted to SQLite from SimpleDB

--- tracerlogger.go
+++ tracerlogger.go
@@ -1,49 +1,91 @@
 package main
 
 import (
-	"encoding/json"
+	"database/sql"
 	"flag"
 	"log"
 	"os"
-	"strconv"
 	"time"
 
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/spagettikod/gotracer"
-	"github.com/spagettikod/sdb"
 )
 
 const (
-	SDBDomainName = "tracerlogger"
+	CreateLogTable string = `
+		CREATE TABLE IF NOT EXISTS log (
+			timestamp			NUMERIC PRIMARY KEY,
+			array_voltage 		REAL,
+			array_current 		REAL,
+			array_power 		REAL,
+			battery_voltage 	REAL,
+			battery_current 	REAL,
+			battery_soc 		INTEGER,
+			battery_temp 		REAL,
+			battery_max_volt 	REAL,
+			battery_min_volt 	REAL,
+			device_temp 		REAL,
+			load_voltage 		REAL,
+			load_current 		REAL,
+			load_power 			REAL,
+			load 				NUMERIC,
+			consumed_day 		REAL,
+			consumed_month 		REAL,
+			consumed_year		REAL,
+			consumed_total 		REAL,
+			generated_day 		REAL,
+			generated_month 	REAL,
+			generated_year		REAL,
+			generated_total 	REAL
+		);
+	`
+	InsertStmt string = `
+		INSERT INTO log (
+			timestamp,
+			array_voltage,
+			array_current,
+			array_power,
+			battery_voltage,
+			battery_current,
+			battery_soc,
+			battery_temp,
+			battery_max_volt,
+			battery_min_volt,
+			device_temp,
+			load_voltage,
+			load_current,
+			load_power,
+			load,
+			consumed_day,
+			consumed_month,
+			consumed_year,
+			consumed_total,
+			generated_day,
+			generated_month,
+			generated_year,
+			generated_total
+		) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);
+	`
 )
 
 var (
-	port, accessKey, secretKey string
-	db                         sdb.SimpleDB
-	buffer                     []*sdb.Item
+	port, dbFile string
+	db           *sql.DB
 )
 
 func init() {
 	flag.StringVar(&port, "p", "", "COM port where EPsolar Tracer is connected")
-	flag.StringVar(&accessKey, "a", "", "AWS Access Key to be able to connect to SimpleDB")
-	flag.StringVar(&secretKey, "s", "", "AWS Secret Key, to be able to connect to SimpleDB")
+	flag.StringVar(&dbFile, "db", "", "path and filename of SQLite database")
 }
 
-func setup() {
-	db = sdb.NewSimpleDB(accessKey, secretKey, sdb.SDBRegionEUWest1)
-	lsr, err := db.ListDomains()
+func setup() (err error) {
+	db, err = sql.Open("sqlite3", dbFile)
 	if err != nil {
-		log.Fatalf("list sdb domains failed: %v", err)
+		return
 	}
-	var exists = false
-	for _, d := range lsr.DomainNames {
-		exists = d == SDBDomainName
-	}
-	if !exists {
-		_, err = db.CreateDomain(SDBDomainName)
-		if err != nil {
-			log.Fatalf("create sdb domain failed: %v", err)
-		}
-	}
+
+	_, err = db.Exec(CreateLogTable)
+	return
 }
 
 func doLog() error {
@@ -52,38 +94,27 @@ func doLog() error {
 		log.Printf("reading tracer status failed: %v", err)
 		return err
 	}
-
-	var b []byte
-	b, err = json.Marshal(t)
-	if err != nil {
-		log.Printf("json marshal of read data failed: %v", err)
-		return err
-	}
-
-	item := sdb.NewItem(strconv.FormatInt(t.Timestamp.Unix(), 10))
-	item.AddAttribute("data", string(b))
-	buffer = append(buffer, item)
-
-	_, err = db.BatchPutAttributes(SDBDomainName, buffer)
-	if err != nil {
-		log.Printf("putting items into simpledb failed, items remain in buffer: %v", err)
-		return err
-	}
-	if len(buffer) > 1 {
-		log.Printf("connection restored to simpledb, sent %v items from buffer", len(buffer))
-	}
-	buffer = []*sdb.Item{}
+	_, err = db.Exec(InsertStmt, t.Timestamp, t.ArrayVoltage, t.ArrayCurrent, t.ArrayPower, t.BatteryVoltage,
+		t.BatteryCurrent, t.BatterySOC, t.BatteryTemp, t.BatteryMaxVoltage, t.BatteryMinVoltage, t.DeviceTemp,
+		t.LoadVoltage, t.LoadCurrent, t.LoadPower, t.Load, t.EnergyConsumedDaily, t.EnergyConsumedMonthly,
+		t.EnergyConsumedAnnual, t.EnergyConsumedTotal, t.EnergyGeneratedDaily, t.EnergyGeneratedMonthly,
+		t.EnergyGeneratedAnnual, t.EnergyGeneratedTotal)
 
 	return err
 }
 
 func main() {
 	flag.Parse()
-	if port == "" || accessKey == "" || secretKey == "" {
+	if port == "" || dbFile == "" {
 		flag.PrintDefaults()
 		os.Exit(-1)
 	}
-	setup()
+	err := setup()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	doLog()
 	c := time.Tick(5 * time.Second)
 	for _ = range c {
